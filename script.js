dcd483b5aee6b6de8681b53ef4fe3e26f8fdfbf3
Kay Putra <kaspr.works@gmail.com>
2022-04-24 18:03:52 +1000

    adding style.css

--- script.js
+++ script.js
@@ -50,5 +50,6 @@ const whereAmI = function(lat, lang) {
 
 
 whereAmI(52.508, 13.381);
-//whereAmI(19.037, 72.873);
-//whereAmI(-33.933, 18.474);
+whereAmI(-33.933, 18.474);
+whereAmI(19.037, 72.873);
+
